1c6102bb33b6da8b0faaf65260e5f4400e42affe
AdityaGaikar91 <gaikaditya795@gmail.com>
2025-08-16 20:51:16 +0530

    Completed setTimeout

---
+++ Asynchronous_Javascript/setTimeout.js
@@ -0,0 +1,40 @@
+// JS is a single threaded synchronous language but use of callbacks, promises, event loop, it can behave asynchronously
+
+// setTimeout is webAPI which allows us to execute code after some delay
+console.log("Hello Javascript");
+
+setTimeout(
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    () => {
+    console.log("Course from Internshala 2");
+},2000);
+
+setTimeout(() => {
+    console.log("Course from Internshala 1");
+},1000);
+
+setTimeout(() => {
+    console.log("Course from Internshala 0");
+}, 0);
+
+fetch('https://dummyjson.com/products/1')
+    .then(res => res.json())
+    .then(json => console.log(json))
+
+console.log("Learning Asynchronous Javascript");
