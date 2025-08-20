32b5543cac13df783972710bc847fb978c06f220
AdityaGaikar91 <gaikaditya795@gmail.com>
2025-08-20 14:33:34 +0530

    Completed async await

--- Asynchronous_Javascript/async_await.js
+++ Asynchronous_Javascript/async_await.js
@@ -1,10 +1,25 @@
-// "https://dummyjson.coms/products/1"
+// "https://dummyjson.com/products/1"
 
 // function fetchData() {
 //     fetch("https://dummyjson.com/products/1")
 //     .then(response => response.json())
 //     .then(result => console.log(result))
+//     .catch(err => console.log(err))
 // }
 
 // fetchData();
 
+// Async / Await 
+
+async function fetchDataResponse() {
+   try {
+    const response = await fetch("https://dummyjson.coms/products/1")
+    const result = await response.json();
+    console.log(result);
+   }catch(error) {
+    console.log(error);
+    
+   }
+}
+
+fetchDataResponse()
