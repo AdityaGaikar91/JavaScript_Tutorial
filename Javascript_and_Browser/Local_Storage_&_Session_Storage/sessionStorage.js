bc53510eb2c5dccbe96f8a4f94077dfdb31e853e
AdityaGaikar91 <gaikaditya795@gmail.com>
2025-08-22 12:06:41 +0530

    Completed session storage

---
+++ Javascript_and_Browser/Local_Storage_&_Session_Storage/sessionStorage.js
@@ -0,0 +1,6 @@
+const field = document.getElementById("textField");
+console.log(field.value);
+
+field.addEventListener("change", function() {
+    sessionStorage.setItem("field", field.value);
+})
